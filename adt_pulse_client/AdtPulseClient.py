c864176c145ddbb0490583fbb8ced0453e5e17cf
mariniertje <mariniertje@gmail.com>
2017-04-21 12:51:04 -0700

    Updated code to get alarm state

--- adt_pulse_client/AdtPulseClient.py
+++ adt_pulse_client/AdtPulseClient.py
@@ -1,9 +1,13 @@
-import zeep
 import logging
+import zeep
+import os.path
 import pickle
 from bs4 import BeautifulSoup
+from dateutil.parser import parse
 import requests
 from requests.auth import AuthBase
+
+_LOGGER = logging.getLogger(__name__)
 
 ARM_TYPE_AWAY = 0
 ARM_TYPE_STAY = 1
@@ -48,38 +52,55 @@ class AdtPulseClient:
     ARMED_STAY = 10203
     ARMED_AWAY = 10201
 
-    def __init__(self, username, password):
-        self.soapClient = zeep.Client(LOGIN_URL)
+    def _save_cookies(requests_cookiejar, filename):
+    """Save cookies to a file."""
+    with open(filename, 'wb') as handle:
+        pickle.dump(requests_cookiejar, handle)
 
-        self.applicationVersion = ADTPULSE_CONTEXT_PATH
-        self.username = username
-        self.password = password
-        self.token = False
 
-        self.locations = []
+    def _load_cookies(filename):
+    """Load cookies from a file."""
+    with open(filename, 'rb') as handle:
+        return pickle.load(handle)
+        
+	def __init__(self, hass, name, username, password, cookie_path):
+		_LOGGER.info('Setting up ADTPulse...')
+		self.usernameForm = username
+		self.passwordForm = password
+		self.cookie_path = cookie_path
+		self.token = False
+		self._state = STATE_UNKNOWN
 
-        self.authenticate()
+		self.authenticate()
 
     def authenticate(self):
         """login to the system"""
+        _LOGGER.info('Logging in to ADTPulse...')
+        session = requests.session()
+        
+        login = session.post(LOGIN_URL, self.usernameForm, self.passwordForm)
+        
+        if login.ResultData == 'Success':
+            self.token = login.SessionID
+#            self.populate_details()
+			_LOGGER.info('Successfully logged in')
 
-        response = self.soapClient.service.AuthenticateUserLogin(self.username, self.password, self.applicationVersion)
-        if response.ResultData == 'Success':
-            self.token = response.SessionID
-            self.populate_details()
         else:
-            Exception('Authentication Error')
+            Exception('Unable to login to portal.adtpulse.com')
+            _LOGGER.info('Unable to login to portal.adtpulse.com')                
+
+
 
     def populate_details(self):
         """populates system details"""
 
-        response = self.soapClient.service.GetSessionDetails(self.token, self.applicationVersion)
+#        response = self._client.GetSessionDetails(self.token)
 
-        logging.info('Getting session details')
+#        _LOGGER.info('Getting session details')
 
-        self.locations = zeep.helpers.serialize_object(response.Locations)['LocationInfoBasic']
+#        self.locations = zeep.helpers.serialize_object(response.Locations)['LocationInfoBasic']
 
-        logging.info('Populated locations')
+#        _LOGGER.info('Populated locations')
 
     def arm_stay(self, location_name=False):
         """arm - stay"""
@@ -94,20 +115,20 @@ class AdtPulseClient:
     def arm(self, arm_type, location_name=False):
         """arm system"""
 
-        location = self.get_location_by_location_name(location_name)
-        deviceId = self.get_security_panel_device_id(location)
+#        location = self.get_location_by_location_name(location_name)
+#        deviceId = self.get_security_panel_device_id(location)
 
-        self.soapClient.service.ArmSecuritySystem(self.token, location['LocationID'], deviceId, arm_type,
+#        self._client.ArmSecuritySystem(self.token, location['LocationID'], deviceId, arm_type,
                                                   '-1')  # Quickarm
 
-        logging.info('armed')
+#        logging.info('armed')
 
     def get_security_panel_device_id(self, location):
         """find the device id of the security panel"""
         deviceId = False
-        for device in location['DeviceList']['DeviceInfoBasic']:
-            if device['DeviceName'] == 'Security Panel':
-                deviceId = device['DeviceID']
+#        for device in location['DeviceList']['DeviceInfoBasic']:
+#            if device['DeviceName'] == 'Security Panel':
+#                deviceId = device['DeviceID']
 
         if deviceId is False:
             raise Exception('No security panel found')
@@ -119,28 +140,38 @@ class AdtPulseClient:
 
         location = False
 
-        for loc in self.locations:
-            if location_name is False and location is False:
-                location = loc
-            elif loc['LocationName'] == location_name:
-                location = loc
+#        for loc in self.locations:
+#            if location_name is False and location is False:
+#                location = loc
+#            elif loc['LocationName'] == location_name:
+#                location = loc
 
         if location is False:
             raise Exception('Could not select location. Try using default location.')
 
         return location
 
-    def get_armed_status(self, location_name=False):
+    def get_armed_status(self, alarm_state_value=False):
         """Get the status of the panel"""
-        location = self.get_location_by_location_name(location_name)
 
-        response = self.soapClient.service.GetPanelMetaDataAndFullStatus(self.token, location['LocationID'], 0, 0, 1)
-
-        status = zeep.helpers.serialize_object(response)
-
-        alarm_code = status['PanelMetadataAndStatus']['Partitions']['PartitionInfo'][0]['ArmingState']
-
-        return alarm_code
+		
+		parsed = BeautifulSoup(login.content, HTML_PARSER)
+		# Find the DIV that contains the current alarm state
+		alarm_state_div = parsed.find_all('div', id = 'divOrbTextSummary')
+		#print (alarm_state_div)
+		# Split the DIV into an array so that we can extract the value
+		alarm_state_array = str(alarm_state_div).split('>')
+		# If we check the array, we can see that the value we need is the 3rd item in the array (DIV-open, SPAN-open, ALARM STATE including SPAN-close, DIV-close)
+		#print (alarm_state_array)
+		# We can easily get rid of the DIV-open tag and the SPAN-open tag, by directly selecting the 3rd value from the array.
+		#print(alarm_state_array[2])
+		# However, this still includes a SPAN-close tag that we need to get rid of
+		# Let's remove the closing SPAN tag as well by splitting this string by the opening of the tag with the '<' character
+		alarm_state_string = str(alarm_state_array[2]).split('<')
+		#print(alarm_state_string)
+		# We can now simply select the cleaned up Alarm State by using the first value out of this new array
+		alarm_state_value = alarm_state_string[0]
+		return alarm_state_value
 
     def is_armed(self, location_name=False):
         """return True or False if the system is alarmed in any way"""
@@ -154,7 +185,7 @@ class AdtPulseClient:
     def disarm(self, location_name=False):
         """disarm the system"""
 
-        location = self.get_location_by_location_name(location_name)
-        deviceId = self.get_security_panel_device_id(location)
+#        location = self.get_location_by_location_name(location_name)
+#        deviceId = self.get_security_panel_device_id(location)
 
-        self.soapClient.service.DisarmSecuritySystem(self.token, location['LocationID'], deviceId, '-1')
+#        self.soapClient.service.DisarmSecuritySystem(self.token, location['LocationID'], deviceId, '-1')
